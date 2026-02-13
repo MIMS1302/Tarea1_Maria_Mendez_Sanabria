4e2331134d6617ef4aa508750d568add04c6185c
MIMS1302 <maria.mendez16@uamcr.net>
2026-02-13 11:11:19 -0600

    Version2

--- Js/main.js
+++ Js/main.js
@@ -1,52 +1,7 @@
 $(document).ready(function () {
 
-    // Mensaje motivador
-    $("#btnMotivar").click(function () {
-        $("#mensaje").fadeToggle();
-    });
-
-    // Noticias
-    $("#btnNoticias").click(function () {
-
-        let noticias = [
-            "Nuevo reto de 30 días",
-            "Competencia regional abierta",
-            "Rutina HIIT avanzada",
-            "Plan nutricional deportivo"
-        ];
-
-        $("#listaNoticias").empty();
-
-        noticias.forEach(function (n) {
-
-            $("#listaNoticias").append(
-                "<li class='list-group-item bg-dark text-white'>" + n + "</li>"
-            );
-
-        });
-
-    });
-
-    // Formulario
-    $("#formContacto").submit(function (e) {
-
-        e.preventDefault();
-
-        let nombre = $("#nombre").val();
-
-        $("#respuesta")
-            .text("Gracias " + nombre + ", pronto te contactamos 💪")
-            .show();
-
-        this.reset();
-
+    $("#btnTema").click(function () {
+        $("body").toggleClass("light-mode");
     });
 
 });
-// Modo claro / oscuro
-$("#btnTema").click(function () {
-
-    $("body").toggleClass("light-mode");
-
-});
-
